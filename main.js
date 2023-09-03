2808485b6bdf1b9674d1b71dd538376685390ffe
HaiTran0303 <haiprotpp@gmail.com>
2023-09-03 22:42:39 +0700

    Add index.html and main.js files

---
+++ main.js
@@ -0,0 +1,23 @@
+var fullName = 'Tran Hoang Hai';
+var age = 26;
+
+
+// alert(fullName);
+// alert(age);
+
+
+//console.log('Day la 1 dong log')
+
+//confirm('Xac nhan') *Dung de hien thi thong bao xac nhan
+
+//prompt('Xac nhan') *Dung de hien thi o message de dien thong tin
+
+// setTimeout(function(){
+//     alert('Thong bao')
+// }, 1000) *Dung de set doan code chay sau 1 thoi gian
+
+
+setInterval(function(){
+    console.log('day la log')
+}, 1000)
+
